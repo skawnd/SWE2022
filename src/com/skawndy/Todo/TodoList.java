0af1a43fbde470be2971091adebf716abe4f0e1e
NJ <vision1993@naver.com>
2017-11-20 22:37:16 +0900

    Todo App 실습 두번째 과제 - 약간 수정

--- src/com/skawndy/Todo/TodoList.java
+++ src/com/skawndy/Todo/TodoList.java
@@ -45,7 +45,7 @@ public class TodoList {
         public void setImage(String image){
             this.image = Image.valueOf(image);
         }
-    }
+}
     public class TodoTask{
         private String Name; // 할 일의 이름도 변경 가능함
         private Date Deadline; // 기한 설정 - Date 클래스 참조함. 현재 날짜와 할 일이 완료되었는지 비교할 것.
@@ -53,7 +53,7 @@ public class TodoList {
         private boolean Finished = false; // 완료여부 - 체크하는 방식
         private boolean myToday = false; // 나의 하루에 추가 - 체크하는 방식
         private Date Alarm; // 미리 알림 - 나중에 시간까지 구체화.
-        private Date Made;
+        private Date Made; // 만들어진 시간 - 정렬 default.
 
         // 할 일 추가할 때 이름만 설정한 다음에 미리 알림, 기한 설정 등을 추가로 설정하는 방식
         // 따라서 생성자에서는 이름과 만든 날짜만 갖고, 나머지는 set메서드를 통해서 할 것.
@@ -82,13 +82,13 @@ public class TodoList {
         public Date getAlarm(){
             return this.Alarm;
         }
+        public Date getMade(){
+            return this.Made;
+        }
         public boolean isAlarm(){ // 미리 알림 기능 : 알람 시간이 지나면 True를 반환하게 함
             if (Alarm == null){ return false; } // 쉴드 패턴.
             Date date = new Date(); // 현재 시간.
             return Alarm.compareTo(date) <= 0;
-        }
-        public Date getMade(){
-            return this.Made;
         }
 
         public void setName(String newname){
@@ -127,8 +127,8 @@ public class TodoList {
 
     public TodoList(String name){
         this.Name = name;
-        TodoTheme theme = new TodoTheme(); // 목록 생성시 자동으로 테마는 랜덤 배정해서 생성.
-        this.isMade = new Date();
+        TodoTheme Theme = new TodoTheme(); // 목록 생성시 자동으로 테마는 랜덤 배정해서 생성.
+        this.isMade = new Date(); // 목록 생성 시간
     }
 
     public void setName(String newname){
@@ -139,7 +139,7 @@ public class TodoList {
         if (Hide == true){ // 완료된 할 일 숨기기
             ArrayList<TodoTask> Tasks = new ArrayList<TodoTask>();
             for (TodoTask t : Origin_Tasks){
-                if (t.getFinished() == true){Tasks.add(t);}
+                if (t.getFinished() == false){Tasks.add(t);}
             }
             return Tasks;
         }else{ // 완료된 할 일 표시.
@@ -150,7 +150,7 @@ public class TodoList {
     public ArrayList<TodoTask> setHide(){
         // 앱에서 누르는 버튼의 의미.
         this.Hide = !Hide;
-        return getTasks();
+        return getTasks(); // 버튼을 누르면 다른 결과가 나온다.
     }
 
     public void sort(int i){
@@ -158,7 +158,7 @@ public class TodoList {
         // 모드별로 비교자 클래스 정의.
         // 일단은 정말 어플처럼 버튼을 통해서 누르는 것처럼 작동하는 느낌으로 1번부터 5번까지의 모드를
         // 선택하는 방식으로 설계를 함. 그러므로 인자로 1부터 5까지의 숫자를 받고 그 외에는 아웃처리함.
-        if (i<=0 | i>5){
+        if (i<=0 | i>5){ // 이 경우 스마트폰에서 해당 버튼 외에 다른 배경이라던지 바깥 부분을 눌러서 종료된 상황을 표현. 즉 변화 없음.
             getTasks();
         }else{
             switch (i){
@@ -183,8 +183,8 @@ public class TodoList {
                     getTasks();
                     break;
 
-                default: // 이름순이 디폴트.
-                    Origin_Tasks.sort(new TodoComparator.Namecomparator());
+                default: // 만들어진 시간 순이 디폴트.
+                    Origin_Tasks.sort(new TodoComparator.MadeComparator());
                     getTasks();
                     break;
             }
